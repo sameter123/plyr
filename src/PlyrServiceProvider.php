756c288a65bcf01736d9c5615dcfe34a9d75fe02
sameter123 <sameter123@users.noreply.github.com>
2023-10-31 13:37:47 +0000

    Fix styling

--- src/PlyrServiceProvider.php
+++ src/PlyrServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Plyr\Plyr\Commands\PlyrCommand;
+use Plyr\Plyr\Testing\TestsPlyr;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Plyr\Plyr\Commands\PlyrCommand;
-use Plyr\Plyr\Testing\TestsPlyr;
 
 class PlyrServiceProvider extends PackageServiceProvider
 {
